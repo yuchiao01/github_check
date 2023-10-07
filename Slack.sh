ea6751e430296d94d7f8b416abbfdfb1623d6481
yuchiao <joy350135@gmail.com>
2023-10-07 10:42:44 +0800

    delete Slack.sh in git

--- Slack.sh
+++
@@ -1,3 +0,0 @@
-#curl -X POST --data-urlencode "payload={\"channel\": \"筆記本\", \"username\": \"Joy-bot\", \"text\": \"打卡\", \"icon_emoji\": \":partyparrot:\"}" https://hooks.slack.com/services/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
-
-curl -X POST --data-urlencode "payload={\"channel\": \"筆記本\", \"username\": \"Joy-bot\", \"text\": \"打卡\", \"icon_emoji\": \":boboowove:\"}" https://hooks.slack.com/services/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
